2bba3e34146365070f235186e17cce6e952d077e
LongHoang77 <longh.772004@gmail.com>
2025-02-28 20:55:07 +0700

    up fe basic map

---
+++ mall-web/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SeatMap from "./components/SeatMap";
+
+function App() {
+  return (
+    <div>
+      <h1>Seat Selection</h1>
+      <SeatMap />
+    </div>
+  );
+}
+
+export default App;
